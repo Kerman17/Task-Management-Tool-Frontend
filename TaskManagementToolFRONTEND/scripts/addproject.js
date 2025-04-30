228ecbb3b59977a0b8db208f8beb577bb3c8fb8e
Detesan Raul <deteraul@yahoo.com>
2025-04-30 14:50:49 +0300

    Styled add new project page.

--- TaskManagementToolFRONTEND/scripts/addproject.js
+++ TaskManagementToolFRONTEND/scripts/addproject.js
@@ -13,28 +13,32 @@ createProjectButton.addEventListener("click", async () => {
         created_by
     };
 
-    console.log(JSON.stringify(projectData));
+    // console.log(JSON.stringify(projectData));
 
-    try {
-        const response = await fetch('http://localhost:8080/api/v1/projects', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(projectData)
-        });
+    if (name && description && created_by) {
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/projects', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(projectData)
+            });
 
-        if (response.ok && name && description && created_by) {
-            alert("Project created successfully!");
+            if (response.ok) {
+                alert("Project created successfully!");
 
-            window.location.href = "index.html";
-            deselectAll();
-            document.querySelector('.js-projects').classList.add("selected");
-        } else {
-            alert("Failed to create project. Please check your input.");
+                window.location.href = "index.html";
+                deselectAll();
+                document.querySelector('.js-projects').classList.add("selected");
+            } else {
+                alert("Failed to create project. Please check your input.");
+            }
+        } catch (error) {
+            console.error("Error creating project:", error);
+
         }
-    } catch (error) {
-        console.error("Error creating project:", error);
-
+    } else {
+        alert("Failed to create project. Please check your input.");
     }
 })
